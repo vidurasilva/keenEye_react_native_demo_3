eb5d8fe2c393a335bbc4c7e90dd94d3bb80ea93e
vidura_silva <vidura.silva@keeneye.solutions>
2022-07-22 17:15:49 +0530

    test git

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to test git start your app!</Text>
+      <Text>Open up App.js to start your app!</Text>
       <StatusBar style="auto" />
     </View>
   );

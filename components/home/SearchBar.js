2178e884734e2c0f8998973a8112bfe5d7de2cab
vidura_silva <vidura.silva@keeneye.solutions>
2022-07-23 23:56:44 +0530

    Add Search Bar with react-native-vector-icons pack

---
+++ components/home/SearchBar.js
@@ -0,0 +1,64 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import AntDesign from "react-native-vector-icons/AntDesign";
+
+export default function SearchBar() {
+  return (
+    <View style={styles.container}>
+      <GooglePlacesAutocomplete
+        placeholder="Search"
+        styles={{
+          textInputContainer: {
+            backgroundColor: "#eee",
+            borderRadius: 50,
+            flexDirection: "row",
+            alignItems: "center",
+            marginRight: 10,
+          },
+          textInput: {
+            backgroundColor: "#eee",
+            borderRadius: 20,
+            fontWeight: "700",
+            marginTop: 7,
+          },
+        }}
+        renderLeftButton={() => (
+          <View style={{ marginLeft: 10 }}>
+            <Ionicons name="location-sharp" size={24} />
+          </View>
+        )}
+        renderRightButton={() => (
+          <View
+            style={{
+              marginRight: 10,
+              flexDirection: "row",
+              backgroundColor: "white",
+              padding: 9,
+              borderRadius: 30,
+              alignItems: "center",
+            }}
+          >
+            <AntDesign
+              name="clockcircle"
+              size={14}
+              style={{ marginRight: 6 }}
+            />
+            <Text>Search</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    marginTop: 15,
+    // backgroundColor: "#eee",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+});
--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,14 @@
 import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
 import HeaderTabs from "../components/home/HeaderTabs";
-// import SearchBar from "../components/home/SearchBar";
+import SearchBar from "../components/home/SearchBar";
 
 export default function Home() {
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       <View style={{ backgroundColor: "#fff", padding: 15 }}>
         <HeaderTabs />
-        {/* <SearchBar /> */}
+        <SearchBar />
       </View>
     </SafeAreaView>
   );

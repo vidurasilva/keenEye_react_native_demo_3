35f143fc7a865b1fd50ee56c73e8282da9b086c7
vidura_silva <vidura.silva@keeneye.solutions>
2022-07-25 16:50:16 +0530

    menu item set data

---
+++ components/restaurantDetails/MenuItems.js
@@ -0,0 +1,87 @@
+import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
+import React from "react";
+import { Divider } from "react-native-elements/dist/divider/Divider";
+
+const foods = [
+  {
+    title: "Lasagna",
+    description: "With butter and mushroom , Tomato and Red chili",
+    price: "1212$",
+    image:
+      "https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg",
+  },
+  {
+    title: "Pizza",
+    description: "With butter and mushroom , Tomato and Red chili",
+    price: "1212$",
+    image:
+      "https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg",
+  },
+  {
+    title: "BBQ",
+    description: "With butter and mushroom , Tomato and Red chili",
+    price: "1212$",
+    image:
+      "https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg",
+  },
+  {
+    title: "Potato Chip",
+    description: "With butter and mushroom , Tomato and Red chili",
+    price: "1212$",
+    image:
+      "https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg",
+  },
+  {
+    title: "Pork curry",
+    description: "With butter and mushroom , Tomato and Red chili",
+    price: "1212$",
+    image:
+      "https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg",
+  },
+];
+
+const styles = StyleSheet.create({
+  menuItemStyle: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 20,
+  },
+
+  titleStyle: {
+    fontSize: 19,
+    fontWeight: "600",
+  },
+});
+
+export default function MenuItems() {
+  return (
+    <ScrollView>
+      {foods.map((food, index) => (
+        <View key={index}>
+          <View style={styles.menuItemStyle}>
+            <FoodInfo food={food} />
+            <FoodImage food={food} />
+          </View>
+          <Divider width={0.5} orientation="vertical" />
+        </View>
+      ))}
+    </ScrollView>
+  );
+}
+
+const FoodInfo = (props) => (
+  <View style={{ width: 240, justifyContent: "space-evenly" }}>
+    <Text style={styles.titleStyle}>{props.food.title}</Text>
+    <Text>{props.food.description}</Text>
+    <Text>{props.food.price}</Text>
+  </View>
+);
+
+const FoodImage = (props) => (
+  <View>
+    <Image
+      source={{ uri: props.food.image }}
+      style={{ width: 100, height: 100, borderRadius: 8 }}
+    />
+  </View>
+);
--- screens/RestaurantDetails.js
+++ screens/RestaurantDetails.js
@@ -2,12 +2,14 @@ import { View, Text } from "react-native";
 import React from "react";
 import About from "../components/restaurantDetails/About";
 import { Divider } from "react-native-elements";
+import MenuItems from "../components/restaurantDetails/MenuItems";
 
 export default function RestaurantDetails() {
   return (
     <View>
       <About />
       <Divider width={1.8} style={{ marginVertical: 20 }} />
+      <MenuItems />
     </View>
   );
 }

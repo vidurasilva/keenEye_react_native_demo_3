717449d3a5fee6966faca9a2db50dd44788e4c96
vidura_silva <vidura.silva@keeneye.solutions>
2022-07-26 10:16:11 +0530

    add redux package and remove yelpRestaurantInfo

--- components/restaurantDetails/About.js
+++ components/restaurantDetails/About.js
@@ -1,16 +1,6 @@
 import { View, Text, Image } from "react-native";
 import React from "react";
 
-const yelpRestaurantInfo = {
-  image:
-    "https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg",
-  name: "Farmhouse Kitchen Thai Cuisine",
-  price: "$$",
-  reviews: "1500",
-  rating: 4.5,
-  category: [{ title: "Thai" }, { title: "Comfort Food " }],
-};
-
 export default function About(props) {
   console.log(props.route.params.image);
   const { name, image, price, reviews, rating, categories } =

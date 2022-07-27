e14a72a298191a2ef5f84194090d3f61facc585a
vidura_silva <vidura.silva@keeneye.solutions>
2022-07-27 22:22:47 +0530

    reducer to reducers

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from "redux";
 import cartReducer from "./cartReducer";
 
-let reducer = combineReducers({
+let reducers = combineReducers({
   cartReducer: cartReducer,
 });
 
 const rootReducer = (state, action) => {
-  return reducer(state, action);
+  return reducers(state, action);
 };
 
 export default rootReducer;
